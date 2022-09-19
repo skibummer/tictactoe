d8fbf17c535d7234aabfb03d3341fea9c6b3910d
skibummer <106363991+skibummer@users.noreply.github.com>
2022-09-19 15:00:40 -0700

    created endgame condition logic, adding documentation

---
+++ endgame.py
@@ -0,0 +1,17 @@
+def end_game_check():
+    if row0[0] == row0[1] == row0[2]:
+        return False
+    elif row1[0] == row1[1] == row2[2]:
+        return False
+    elif row2[0] == row2[1] == row2[2]:
+        return False
+    elif row0[0] == row1[0] == row2[0]:
+        return False
+    elif row0[1] == row1[1] == row2[1]:
+        return False
+    elif row0[2] == row1[2] == row2[2]:
+        return False
+    elif turns_taken == 9:
+        return False
+    else:
+        return True
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+#imports
+from endgame import end_game_check
+
+# Start Conditions
 row0 = ["⬜️", "⬜️", "⬜️"]
 row1 = ["⬜️", "⬜️", "⬜️"]
 row2 = ["⬜️", "⬜️", "⬜️"]
 map = [row0, row1, row2]
 print("     Column0  Column1  Column2")
 print(f"Row0{row0}\nRow1{row1}\nRow2{row2}")
+game_continue = True
+turns_taken = 0
 
-# TODO 1. Add multiple players
+# TODO 1. Account for a choice already being taken
 # TODO 2. Add end game conditions
 # TODO 3. UX improvements to prevent user from crashing game
 # TODO 4. UI
@@ -13,28 +19,26 @@ print(f"Row0{row0}\nRow1{row1}\nRow2{row2}")
 # TODO
 # TODO
 
-while True:
+while game_continue:
+    # Player 1's turn
+    p1_column_choice = int(input("Player 1: Choose the column of your move. \n "))
+    p1_row_choice = int(input("Player 1: Choose the row of your move.\n "))
 
-    p1_x_cord = int(input("Player 1: Type the X coordinate of your move. \n "))
-    p1_y_cord = int(input("Player 1: Type the Y coordinate of your move.\n "))
-
-    map[p1_y_cord][p1_x_cord] = "X    "
-
+    map[p1_row_choice][p1_column_choice] = "X    "
+    turns_taken += 1
+    print(f"turns_taken: {turns_taken}")
+    #Show's update of player 1's move
     print("     Column0  Column1  Column2")
     print(f"Row0{row0}\nRow1{row1}\nRow2{row2}")
 
-    p2_x_cord = int(input("Player 2: Type the X coordinate of your move. \n "))
-    p2_y_cord = int(input("Player 2: Type the Y coordinate of your move.\n "))
 
-    map[p2_y_cord][p2_x_cord] = "O    "
+    #Player 2's Move
+    p2_column_choice= int(input("Player 2: Choose the column of your move. \n "))
+    p2_row_choice = int(input("Player 2: Choose the row of your move.\n "))
 
+    map[p2_row_choice][p2_column_choice] = "O    "
+    #records player 2's move
+    turns_taken += 1
+    #Shows update of player 2's move
     print("     Column0  Column1  Column2")
     print(f"Row0{row0}\nRow1{row1}\nRow2{row2}")
-
-#
-# column_chosen = int(position[1])-1
-# row_chosen = int(position[0])-1
-# map[(column_chosen)][(row_chosen)] = "X"
-
-# print(f"{row0}\n{row1}\n{row2}")
-
