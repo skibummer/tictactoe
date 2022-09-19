4fbbd67aecc67314c0eba7a1eb241fc7a1d61001
skibummer <106363991+skibummer@users.noreply.github.com>
2022-09-19 15:16:09 -0700

    updated endgame condition logic prevents win conditions from triggering by "empty" coordinates

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-#imports
-from endgame import end_game_check
+def end_game_check():
+    if row0[0] == row0[1] == row0[2] and row0[0] != "⬜️":
+        return False
+    elif row1[0] == row1[1] == row2[2] and row0[0] != "⬜️":
+        return False
+    elif row2[0] == row2[1] == row2[2] and row0[0] != "⬜️":
+        return False
+    elif row0[0] == row1[0] == row2[0] and row0[0] != "⬜️":
+        return False
+    elif row0[1] == row1[1] == row2[1] and row0[0] != "⬜️":
+        return False
+    elif row0[2] == row1[2] == row2[2] and row0[0] != "⬜️":
+        return False
+    elif turns_taken == 9:
+        return False
+    else:
+        return True
 
 # Start Conditions
 row0 = ["⬜️", "⬜️", "⬜️"]
@@ -15,11 +30,12 @@ turns_taken = 0
 # TODO 2. Add end game conditions
 # TODO 3. UX improvements to prevent user from crashing game
 # TODO 4. UI
-# TODO
+# TODO 5. add that rows and columns must be "X" or "O" ,not
 # TODO
 # TODO
 
 while game_continue:
+    game_continue = end_game_check()
     # Player 1's turn
     p1_column_choice = int(input("Player 1: Choose the column of your move. \n "))
     p1_row_choice = int(input("Player 1: Choose the row of your move.\n "))
@@ -39,6 +55,7 @@ while game_continue:
     map[p2_row_choice][p2_column_choice] = "O    "
     #records player 2's move
     turns_taken += 1
+    print(f"turns_taken: {turns_taken}")
     #Shows update of player 2's move
     print("     Column0  Column1  Column2")
     print(f"Row0{row0}\nRow1{row1}\nRow2{row2}")
