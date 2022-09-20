83da4bb52588fa110ce3241808b097e35fd43295
skibummer <106363991+skibummer@users.noreply.github.com>
2022-09-20 09:42:21 -0700

    got rid of empty endgame.py, ready to push

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def end_game_check():
         else:
             print("Player 2 wins!")
         return False
+
     elif row0[1] == row1[1] == row2[1]  and row0[1] != "⬜️":
         if row0[1] == "X    ":
             print("Player 1 wins!")
@@ -62,7 +63,6 @@ def end_game_check():
     else:
         return True
 
-
 # Start Conditions
 row0 = ["⬜️", "⬜️", "⬜️"]
 row1 = ["⬜️", "⬜️", "⬜️"]
@@ -73,18 +73,15 @@ print(f"Row0{row0}\nRow1{row1}\nRow2{row2}")
 game_continue = True
 turns_taken = 0
 player_turn = "1"
-# TODO 1. Account for a choice already being taken
-# xTODO 2. Add end game conditions
-# TODO 3. UX/UI improvements to prevent user from crashing game
-# TODO 4. add that rows and columns must be "X" or "O" ,not empty
-# TODO
-# TODO
+
+# TODO UX/UI improvements to prevent user from crashing game
+
 
 while game_continue:
-
+    #Player 1 Turn
     if player_turn == "1" and game_continue == True:
-        p1_row_choice = int(input("Player 1: Choose the row of your move.\n "))
-        p1_column_choice = int(input("Player 1: Choose the column of your move. \n "))
+        p1_row_choice = int(input("Player 1: Choose the row of your move. ('0', '1', or '2')\n "))
+        p1_column_choice = int(input("Player 1: Choose the column of your move. ('0', '1', or '2')\n "))
 
         # checking if choice is valid.
         if p1_column_choice < 0 or p1_column_choice > 2 or p1_row_choice < 0 or  p1_row_choice > 2:
@@ -93,22 +90,21 @@ while game_continue:
         # checking if choice is already taken.
         elif map[p1_row_choice][p1_column_choice] != "⬜️":
             print("That box is already chosen, pick an empty coordinate")
-        #     have player to pick a different spot
-        else:
 
+        # Show's update of player 1's move
+        else:
             map[p1_row_choice][p1_column_choice] = "X    "
             turns_taken += 1
             player_turn = "2"
             game_continue = end_game_check()
-            #Show's update of player 1's move
             print("     Column0  Column1  Column2")
             print(f"Row0{row0}\nRow1{row1}\nRow2{row2}")
 
 
     #Player 2's Move
     if player_turn == "2" and game_continue == True:
-        p2_row_choice = int(input("Player 2: Choose the row of your move.\n "))
-        p2_column_choice = int(input("Player 2: Choose the column of your move. \n "))
+        p2_row_choice = int(input("Player 2: Choose the row of your move.('0', '1', or '2')\n "))
+        p2_column_choice = int(input("Player 2: Choose the column of your move. ('0', '1', or '2')\n "))
 
         if p2_column_choice < 0 or p2_column_choice > 2 or p2_row_choice < 0 or  p2_row_choice > 2:
             print("Try again")
