1991ce8cfe00a9cca215fd555e46e876893f1da1
skibummer <106363991+skibummer@users.noreply.github.com>
2022-10-10 14:30:03 -0700

    Fix string syntax on line 95 and 99 Add comments to code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def game_core():
-
+    """TicTacToe"""
     def end_game_check():
         """"Defines and checks end of game conditions"""
         if row0[0] == row0[1] == row0[2] and row0[0] != "____":
@@ -65,6 +65,7 @@ def game_core():
         else:
             return True
 
+    # Set start conditions
     row0 = ["____", "____", "____"]
     row1 = ["____", "____", "____"]
     row2 = ["____", "____", "____"]
@@ -75,6 +76,7 @@ def game_core():
     print("_____Column0_Column1_Column2")
     print(f"Row0{row0}\nRow1{row1}\nRow2{row2}")
 
+    # Game loop/core
     while game_continue:
 
         # check if Player 1 Turn
@@ -88,14 +90,15 @@ def game_core():
             # test
             print(f"{p1_coordinate_choice}")
 
-            # checking if choice is valid.
+            # check if input is valid character length
             if len(p1_coordinate_raw) != 2:
-                print("Invalid input, will you please? try again")
+                print("Invalid input, will you please try again?")
 
+            # check if coordinate given is within map range
             elif p1_column_choice < 0 or p1_column_choice > 2 or p1_row_choice < 0 or p1_row_choice > 2:
-                print("Invalid input, will you please? try again")
+                print("Invalid input, will you please try again?")
 
-            # checking if choice is already taken.
+            # check if choice is already taken
             elif game_map[p1_row_choice][p1_column_choice] != "____":
                 print("That box is already chosen, pick an empty coordinate")
 
@@ -117,13 +120,16 @@ def game_core():
             p2_column_choice = int(p2_coordinate_choice[1])
             print(f"{p2_coordinate_choice}")
 
+            # check if input is valid character length
             if len(p2_coordinate_raw) != 2:
                 print("Invalid input, will you please? try again")
 
+            # check if coordinate given is within map range
             elif p2_column_choice < 0 or p2_column_choice > 2 or p2_row_choice < 0 or p2_row_choice > 2:
                 print(p2_row_choice, p2_column_choice)
                 print("Invalid input, will you please? try again")
 
+            # check if choice is already taken
             elif game_map[p2_row_choice][p2_column_choice] != "____":
                 print("That box is already chosen, pick an empty coordinate")
 
@@ -140,7 +146,7 @@ def game_core():
         # Ask for restart after game ends, resets to start conditions
         if not game_continue:
             if input("Would you like to play again? y/n: ") == "y":
-                new_game()
+                game_core()
             else:
                 print("Goodbye")
 game_core()
