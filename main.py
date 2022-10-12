86d30cacf5b4ccbc696d813e940d3997472a3550
skibummer <106363991+skibummer@users.noreply.github.com>
2022-10-11 20:07:32 -0700

    Finish conversion to cartesian coordinates

--- main.py
+++ main.py
@@ -88,7 +88,6 @@ def game_core():
         # test
         # print(f"player_turn {player_turn}")
         # print(f"turns_taken {turns_taken}")
-
         if player_turn == "1":
             game_map[y_coord][x_coord] = "_X__"
 
@@ -107,8 +106,8 @@ def game_core():
     turns_taken = 0
     player_turn = "1"
 
-    print(f"Row3{row3}\nRow2{row2}\nRow1{row1}")
-    print("_____Column1_Column2_Column3")
+    print(f"3{row3}\n2{row2}\n1{row1}")
+    print("_____1______2_______3____")
 
     # game loop/core
     while game_continue:
