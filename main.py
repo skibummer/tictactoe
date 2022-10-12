ff3da2dd9110eb25437657e33fbd6a8c47e7e60d
skibummer <106363991+skibummer@users.noreply.github.com>
2022-10-12 11:13:26 -0700

    Regain mark_map functionality for player 1, doesn't change turn to player 2 though.

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ class Game():
                 print("Player 2 wins!")
             return False
 
-        elif turns_taken == 9:
+        elif self.turns_taken == 9:
             print("Cats game!")
             return False
 
@@ -127,14 +127,14 @@ class Game():
                 # print(f"test p1_x_coord: {p1_x_coord}")
                 # print(f"test p1_y_coord: {p1_y_coord}")
 
-                if valid_input_check(p1_coord_raw, p1_x_coord, p1_y_coord):
+                if self.valid_input_check(p1_coord_raw, p1_x_coord, p1_y_coord):
                     """If input is somewhat valid, finish player 1 turn"""
-                    mark_map(p1_x_coord, p1_y_coord)
-                    turns_taken += 1
+                    self.mark_map(p1_x_coord, p1_y_coord)
+                    self.turns_taken += 1
                     # print(f"turns_taken:{turns_taken}")
                     player_turn = "2"
-                    game_continue = end_game_check()
-                    print(f"Row3{row3}\nRow2{row2}\nRow1{row1}")
+                    game_continue = self.end_game_check()
+                    print(f"Row3{self.row3}\nRow2{self.row2}\nRow1{self.row1}")
                     print("_____Colum1_Column2_Column3")
 
             # check if Player 2's turn
@@ -147,23 +147,24 @@ class Game():
                 # test input
                 # print(p2_coord_raw)
 
-                if valid_input_check(p2_coord_raw, p2_x_coord, p2_y_coord):
+                if self.valid_input_check(p2_coord_raw, p2_x_coord, p2_y_coord):
                     # record player 2's move and check end of game conditions
-                    mark_map(p2_x_coord, p2_y_coord)
-                    turns_taken += 1
-                    print(turns_taken)
+                    self.mark_map(p2_x_coord, p2_y_coord)
+                    self.turns_taken += 1
+                    print(self.turns_taken)
                     player_turn = "1"
-                    game_continue = end_game_check()
-                    print(f"Row3{row3}\nRow2{row2}\nRow1{row1}")
+                    game_continue = self.end_game_check()
+                    print(f"Row3{self.row3}\nRow2{self.row2}\nRow1{self.row1}")
                     print("_____Column1_Column2_Column3")
 
             # ask for restart after game ends, resets to start conditions if yes
             if not game_continue:
                 if input("Would you like to play again? y/n: ") == "y":
-                    game_core()
+                    self.game_core()
                 else:
                     print("Goodbye")
 
 new_game = Game()
 
 new_game.print_map()
+new_game.game_core()
